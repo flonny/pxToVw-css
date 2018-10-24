1bcc51b563d06300ebd629817b8a5f6e0cfae52b
fanyuedong <632296950@qq.com>
2018-10-25 01:46:21 +0800

    gulp升级

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,32 @@ const autoprefixer = require('gulp-autoprefixer');
 const browsersync = require('browser-sync').create();
 const eslint = require('gulp-eslint');
 
-gulp.task('default', ['styles', 'script'], () => {
-  gulp.watch('./sass/**/*.scss', ['styles']);
-  gulp.watch('./js/**/*.js', ['script']);
-  browsersync.init({
-    watch: true,
-    server: './',
-  });
-});
 
-gulp.task('styles', () => {
+gulp.task('styles', (done) => {
   gulp.src('./sass/**/*.scss')
-    .pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(sass.sync({
+      outputStyle: 'compressed',
+    }).on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
     }))
     .pipe(gulp.dest('./css'));
+  done();
 });
-gulp.task('script', () => {
+gulp.task('script', (done) => {
   gulp.src('./js/**/*.js')
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failOnError());
+  done();
 });
+
+gulp.task('default', gulp.series('styles', 'script', (done) => {
+  gulp.watch('./sass/**/*.scss', gulp.series('styles'));
+  gulp.watch('./js/**/*.js', gulp.series('script'));
+  browsersync.init({
+    watch: true,
+    server: './',
+  });
+  done();
+}));
