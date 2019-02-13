f492679ea6aad3cf8861bf9b1339722859dc4b09
fanyuedong <632296950@qq.com>
2019-02-13 20:40:05 +0800

    vue

---
+++ vue/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button-counter></button-counter>
+  </div>
+</template>
+<script>
+  import Vue from 'vue'
+  Vue.component('button-counter',{
+    name: 'buttonCounter',
+    data() {
+      return {
+        count: 0
+      }
+    },
+    templete: `<button @click="count++">you click me {{count}} times</button>`
+  })
+export default {
+    components: {
+        buttonCounter
+    }
+}
+</script>
